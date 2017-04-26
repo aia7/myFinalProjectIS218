2d50c1018fcf98063678b8ebfaff5c894f61b087
aia7 <aia7@njit.edu>
2017-04-26 19:26:07 -0400

    removed class attribute

--- user.php
+++ user.php
@@ -56,7 +56,7 @@
 
         <div class="row">
             <div class="col-lg-12 text-center">
-                <h1><p align="left">Incomplete :</p></h1><input class="button" type="button" value="ADD"><br>
+                <h1><p align="left">Incomplete :</p></h1><input type="button" value="ADD"><br>
 
                 <br>
                 <br>
