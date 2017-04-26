08b144dafe77fd906f404144879fdafc9a777d71
aia7 <aia7@njit.edu>
2017-04-26 15:34:34 -0400

    test

--- insertsi.php
+++ insertsi.php
@@ -1,6 +1,9 @@
 <?php
-
+// if(isset($_POST['submit']))
+require('database.php');
+{
 	$fname=$_POST['fname'];
+	echo $fname;
 	$lname=$_POST['lname'];
 	$email=$_POST['email'];
 	$phone=$_POST['phone'];
@@ -8,5 +11,11 @@
 	$gender=$_POST['gender'];
 	$password=$_POST['password'];
 
-	$sql="INSERT INTO acc (fname,lname,email,phone,birthday,gender,password) VALUES ('$fname','$lname','$email','$phone','$birthday','$gender','$password')";
+	$sql="INSERT INTO acc (fname,lname,email,phone,birthday,gender,password) VALUES (
+	'$fname','$lname','$email','$phone','$birthday','$gender','$password')";
+
+	if(mysql_query($sql)){
+		echo "<h3> Student's data is inserted </h3>";
+	}
+}
 	?>
