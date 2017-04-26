48dbbf93544fd492358c66df0ecf1858d7582384
aia7 <aia7@njit.edu>
2017-04-26 11:01:26 -0400

    created this file to insert the form data into the database

---
+++ insertsi.php
@@ -0,0 +1,12 @@
+<?php
+
+	$fname=$_POST['fname'];
+	$lname=$_POST['lname'];
+	$email=$_POST['email'];
+	$phone=$_POST['phone'];
+	$birthday=$_POST['birthday'];
+	$gender=$_POST['gender'];
+	$password=$_POST['password'];
+
+	$sql="INSERT INTO acc (fname,lname,email,phone,birthday,gender,password) VALUES ('$fname','$lname','$email','$phone','$birthday','$gender','$password')";
+	?>
