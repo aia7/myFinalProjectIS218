182dadd35f016c9be275563340dff01c9160ef03
aia7 <aia7@njit.edu>
2017-04-25 20:39:53 -0400

    set up the code to connect to the database

--- database.php
+++ database.php
@@ -3,17 +3,16 @@
 	date_default_timezone_set('America/New_York');
 	
 	
-	//$dsn = 'mysql:host=sql1.njit.edu;dbname=aia7';
+	
 	$hostname = 'sql1.njit.edu';
 	$username = 'aia7';
 	$password = 'qGd11eane';
 	$database = 'aia7';
 
-	
 
 	try {
         
-        //$db = new PDO($dsn, $username, $password);
+        //$db = new PDO("$hostname", "$username", "$password","$database");
         $db = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
         
     } catch (PDOException $e) {
