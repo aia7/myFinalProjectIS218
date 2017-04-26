4fab16871dd4bc134690302fc6d3ac0ac802310d
aia7 <aia7@njit.edu>
2017-04-26 18:48:30 -0400

    changed alignment tag

--- user.php
+++ user.php
@@ -54,7 +54,7 @@
 
         <div class="row">
             <div class="col-lg-12 text-center">
-                <p align="left"><h1>Incomplete :</h1>
+                <p align="left">Incomplete :</p>
             </div>
         </div>
        
