802ba1a31d25564c358ca901f3da39bb26c89a99
aia7 <aia7@njit.edu>
2017-04-26 19:32:24 -0400

    added cdn and button

--- user.php
+++ user.php
@@ -9,7 +9,9 @@
    
 
     <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
@@ -56,7 +58,7 @@
 
         <div class="row">
             <div class="col-lg-12 text-center">
-                <h1><p align="left">Incomplete :</p></h1><input type="button" value="ADD"><br>
+                <h1><p align="left">Incomplete :</p></h1><button type="button" class="btn btn-outline-danger">ADD</button><br>
 
                 <br>
                 <br>
