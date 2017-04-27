3c8e3d1a37438db4e8beb9c6862c4950beb18ee0
aia7 <aia7@njit.edu>
2017-04-27 12:36:19 -0400

    changed input fields for the task form

--- taskinsert.php
+++ taskinsert.php
@@ -1,17 +1,140 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+   
+
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <style>
+    body {
+        padding-top: 70px;
+    }
+    </style>
+
+</head>
+
+<body>
+
+<?php
+
+    require_once 'database.php';
+    //pdoconnect is used to connect to the database
+    $query = 'SELECT * FROM todo ORDER BY due ASC';
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $results = $statement->fetchAll();
+    $statement->closeCursor();
+
+foreach($results as $row) { ?>
+<div class="task">
+        <h3><?php echo $row['task']?></h3>
+    <p>
+        First Name : <span><?php echo $row['FirstName']?></span>
+        <br>
+        Description: <span><?php echo $row['Description'] ?></span>
+        <br>
+        
+
+
+
+
+?>
+
+
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div c lass="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a onclick=window.location.href="mainpage.html" class="navbar-brand" >TODO LIST</a>
+                <a onclick=window.location.href="mainpage.html" class="navbar-brand" > Log Out</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h1><p align="left">Incomplete :</p></h1><button type="button" onclick=window.location.href="task.php" class="btn btn-outline-danger" style="background-color: #ff0000; color: #ffffff" >+ Add A New Task</button><br>
+
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                 <h1><p align="left">Complete :</p></h1>
+
+            </div>
+        </div>
+       
+
+    </div>
+
+
+
+    <!-- jQuery Version 1.11.1 -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
+
 <?php
 // if(isset($_POST['submit']))
 require('database.php');
 
 {
-	$fname=$_POST['FirstName'];
 	$dname=$_POST['Description'];
 	$start=$_POST['StartDate'];
-	$message=$_POST['message'];
+	$due=$_POST['duedate'];
 
 	
 
-	$sql="INSERT INTO todo (FirstName,Description,StartDate,message) VALUES (
-	'$fname','$dname','$start','$message')";
+	$sql="INSERT INTO todo (Description,StartDate,duedate) VALUES (
+	$dname','$start','$due)";
 
 
 	$statement = $db->prepare($sql);
